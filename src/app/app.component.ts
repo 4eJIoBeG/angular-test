833a13b7540509c0d0e292706f1177de1af53285
4eJIoBeG <pavelshashenko@gmail.com>
2025-02-26 14:32:38 +0300

    add routes, add login page, add search page, add profile page, add custom margin scss file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
-import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((value) => {
-      this.profiles = value;
-    });
-  }
-}
+export class AppComponent {}
