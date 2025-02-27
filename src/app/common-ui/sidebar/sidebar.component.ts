96f38108fe56d6fee034c2f8ae1b09a914490f6f
4eJIoBeG <pavelshashenko@gmail.com>
2025-02-27 16:37:01 +0300

    add sidebar, add method in profileServices for getSubsctibers

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
+import { RouterLink } from '@angular/router';
+import { ProfileService } from '../../data/services/profile.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SvgIconComponent, SubscriberCardComponent, RouterLink, AsyncPipe],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService);
 
+  subscribers$ = this.profileService.getSubscribersShortList();
+
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: '/chats',
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: '/search',
+    },
+  ];
 }
